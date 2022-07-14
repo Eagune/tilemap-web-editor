58621795669d5f7e47110c5785b7d7ee473fd012
Eagune 胡义晋 <huyj52@cn.midea.com>
2022-07-14 10:06:34 +0800

    rollup esm umd

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,40 @@
 import path from 'path'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import pkg from './package.json'
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
+const name = 'TilemapWebEditor'
 
-export default {
-  input: path.resolve(__dirname, './src/index.js'),
-  output: {
-    sourcemap: true,
-    file: 'dist/index.js',
-    format: 'esm',
-    name: 'tilemapWebEditor'
+const input = path.resolve(__dirname, './src/index.js')
+const plugins = [
+  resolve({
+    browser: true,
+    mainFields: ['module', 'main'],
+    extensions,
+  }),
+  commonjs(),
+]
+
+export default [
+  {
+    input,
+    output: {
+      sourcemap: true,
+      file: pkg.module,
+      format: 'esm',
+      name,
+    },
+    plugins
   },
-  plugins: [
-    resolve({
-      browser: true,
-      mainFields: ['module', 'main'],
-      extensions,
-    }),
-    commonjs(),
-  ]
-}
+  {
+    input,
+    output: {
+      sourcemap: true,
+      file: pkg.main,
+      format: 'umd',
+      name,
+    },
+    plugins
+  }
+]
