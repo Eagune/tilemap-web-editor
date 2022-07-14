a7ebde78b51f0ea35d93ce6ec4c70e341a3a06c8
Eagune 胡义晋 <huyj52@cn.midea.com>
2022-07-14 10:27:28 +0800

    typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,18 @@
 import path from 'path'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
 const name = 'TilemapWebEditor'
 
-const input = path.resolve(__dirname, './src/index.js')
+const input = path.resolve(__dirname, './src/index.ts')
 const plugins = [
+  typescript({
+    clean: true,
+    tsconfig: path.resolve(__dirname, './tsconfig.json'),
+  }),
   resolve({
     browser: true,
     mainFields: ['module', 'main'],
--- src/index.js
+++
@@ -1,7 +0,0 @@
-function TilemapWebEditor() {
-
-}
-
-export {
-  TilemapWebEditor
-}
